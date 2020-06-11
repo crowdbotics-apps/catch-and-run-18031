eb19def99d845f4c219e2f30a85a0781d2703a60
Crowdbotics <team@crowdbotics.com>
2020-06-11 22:49:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/catch_and_run_18031/MainActivity.java
@@ -0,0 +1,15 @@
+package com.catch_and_run_18031;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "catch_and_run_18031";
+    }
+}
